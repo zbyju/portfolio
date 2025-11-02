86e8bc414206bcfede49266394ee685375df91de
Zbyněk Juřica <zbynek.jurica@gmail.com>
2025-11-02 13:38:18 +0100

    feat: update experience content

--- src/content/cv/work.ts
+++ src/content/cv/work.ts
@@ -1,12 +1,41 @@
 export const work = [
-	{
-		job_title: "Fullstack Developer & System Administrator",
-		company: "Náměšť nad Oslavou, MONTEL",
-		country: "Czech Republic",
-		workload: "Part Time",
-		start: new Date(2016, 4),
-		end: "Present",
-		content: `<ul class="ml-8 list-disc">
+  {
+    job_title: "Backend Engineer",
+    company: "Zurich, grape insurance",
+    country: "Switzerland",
+    workload: "Full time",
+    start: new Date(2024, 10),
+    end: "Present",
+    content: `<ul class="ml-8 list-disc">
+      <li>Collaborated with internal claiming team on improving their operations delivering key functionalities for their day-to-day tasks and quality of life features.</li>
+      <li>Developed a payment solution including integration with UBS and automatic calculation of claims</li>
+    </ul>
+    `,
+    keywords: [
+      "TypeScript",
+      "Node.js",
+      "PostgreSQL",
+      "Domain-driven design (DDD)",
+      "Event-driven architecture",
+      "Google Cloud Platform",
+      "Pubsub",
+      "Next.js",
+      "React",
+      "Redis",
+      "Docker",
+      "Docker-Compose",
+      "Retool",
+      "Graphite",
+    ],
+  },
+  {
+    job_title: "Fullstack Developer & System Administrator",
+    company: "Náměšť nad Oslavou, MONTEL",
+    country: "Czech Republic",
+    workload: "Part Time",
+    start: new Date(2016, 4),
+    end: "Present",
+    content: `<ul class="ml-8 list-disc">
     <li>Created key web applications for the <a href="https://en.wikipedia.org/wiki/Baby_hatch" class="underline">Babybox project</a> in Czechia:</li>
       <ol class="ml-8 list-decimal">
         <li>Designed and implemented a comprehensive application to gather, analyze, and visualize data from over 80 babyboxes, complete with real-time notifications and managing all babybox data and documents allowing for higher efficiency of the operational and maintenance staff</li>
@@ -17,57 +46,58 @@ export const work = [
       <li>Collaborated directly with hospital staff and network administrators to ensure seamless integration of each Babybox into the hospital's network</li>
       <li>Created and managed the company's official website, focusing on accessibility, responsiveness, SEO optimization</li>
       <li>Managed the company's network and server infrastructure, ensuring high uptime and availability</li>
+    </ul>
     `,
-		keywords: [
-			"TypeScript",
-			"JavaScript",
-			"Node.js",
-			"Go",
-			"Python",
-			"React",
-			"Vue.js",
-			"Next.js",
-			"Astro",
-			"Docker",
-			"MongoDB",
-			"InfluxDB",
-			"RabbitMQ",
-		],
-	},
-	{
-		job_title: "Software Engineer",
-		company: "GEM Systems",
-		country: "Prague, Czech Republic",
-		workload: "Internship",
-		start: new Date(2020, 2),
-		end: new Date(2021, 0),
-		content: `<ul class="ml-8 list-disc">
+    keywords: [
+      "TypeScript",
+      "JavaScript",
+      "Node.js",
+      "Go",
+      "Python",
+      "React",
+      "Vue.js",
+      "Next.js",
+      "Astro",
+      "Docker",
+      "MongoDB",
+      "InfluxDB",
+      "RabbitMQ",
+    ],
+  },
+  {
+    job_title: "Software Engineer",
+    company: "GEM Systems",
+    country: "Prague, Czech Republic",
+    workload: "Internship",
+    start: new Date(2020, 2),
+    end: new Date(2021, 0),
+    content: `<ul class="ml-8 list-disc">
   <li>Contributed to analysis and development processes for GEM Winch, a comprehensive data anonymization tool</li>
   <li>Designed and implemented a JavaFX-based user interface for a filesystem anonymization application, enabling intuitive file selection and configuration of anonymization parameters</li>
   <li>Engineered advanced anonymization functionalities for Excel files, allowing precise selection and anonymization of specific cells or ranges</li>
 </ul>`,
-		keywords: [
-			"Groovy",
-			"Java",
-			"PostgreSQL",
-			"Oracle SQL",
-			"MySQL",
-			"DB2",
-			"JavaFX",
-		],
-	},
-	{
-		job_title: "Web Designer",
-		company: "Pedro Resina",
-		country: "Montijo, Portugal",
-		workload: "High School Internship",
-		start: new Date(2017, 8),
-		end: new Date(2017, 8),
-		content: `<ul class="ml-8 list-disc">
+    keywords: [
+      "Groovy",
+      "Java",
+      "PostgreSQL",
+      "Oracle SQL",
+      "MySQL",
+      "DB2",
+      "JavaFX",
+    ],
+  },
+  {
+    job_title: "Web Designer",
+    company: "Pedro Resina",
+    country: "Montijo, Portugal",
+    workload: "High School Internship",
+    start: new Date(2017, 8),
+    end: new Date(2017, 8),
+    content: `<ul class="ml-8 list-disc">
 <li>Focused on optimizing WordPress templates for improved responsiveness and user experience</li>
 <li>These templates were successfully deployed for client websites, resulting in positive feedback</li>
       </ul>
     `,
-		keywords: ["HTML", "CSS", "JavaScript", "Wordpress", "PHP"],
-	},
+    keywords: ["HTML", "CSS", "JavaScript", "Wordpress", "PHP"],
+  },
 ];
